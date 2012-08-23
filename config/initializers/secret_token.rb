3555ce30bfd60a74962e9b3ee050498b9fa840fb
João Ferreira Loff <jfloff@gmail.com>
2012-08-24 00:06:52 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bvl::Application.config.secret_token = 'af607334aa496f4c3de9e8c3896f7bac338a369a11da3fe9069692a2fc597f0d96ea4a5ceccd2389779b943e4effee9345d9d1a8f79fbef31f090b3370c1bf7e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bvl::Application.config.session_store :cookie_store, key: '_bvl_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bvl::Application.config.session_store :active_record_store
